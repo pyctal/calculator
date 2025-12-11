1ba4638fce2068e6133e044ad78d31b142058f67
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-11 12:55:08 +0530

    Refactor variable names and fix syntax errors
    Rename function parameters and global state variables to use more
    descriptive identifiers.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 "use strict";
 
-let x, y, operator;
+let firstNumber, secondNumber, operator;
 
-function add(x, y) {
-  return x + y;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(x, y) {
-  return x - y;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multipy(x, y) {
-  return x * y;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(x, y) {
-  return x / y;
+function divide(a, b) {
+  return a / b;
 }
